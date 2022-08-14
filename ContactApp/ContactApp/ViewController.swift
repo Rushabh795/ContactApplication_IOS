ee96dd7e76bb5823d4c6500e6e222b9ce4f05556
Rushabh Shah <rushabh795@gmail.com>
2022-08-14 16:14:56 -0400

    First Commit
    - Project Added

---
+++ ContactApp/ContactApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactApp
+//
+//  Created by Rushabh Shah on 2022-08-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
